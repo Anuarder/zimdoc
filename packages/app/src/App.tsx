f27ccc791f19a828ac0fe911f284beaf42d230ad
Anuar Ibrayev <a.ibrayev@chocotravel.com>
2023-11-28 17:34:59 +0600

    feat(app): add protected main and document pages

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,10 +1,42 @@
-import { Routes, Route } from 'react-router-dom';
+import type { JSX } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { User } from '~/entities/user';
 import { AuthPage } from './pages/auth-page';
+import { MainPage } from './pages/main-page';
+import { DocumentPage } from './pages/document-page';
+
+function ProtectedRoute({
+  children,
+}: {
+  children?: string | JSX.Element | JSX.Element[];
+}) {
+  if (!User.isAuthenticated()) {
+    return <Navigate to="/auth" replace />;
+  }
+
+  return children;
+}
 
 function App() {
   return (
     <>
       <Routes>
+        <Route
+          index
+          element={
+            <ProtectedRoute>
+              <MainPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/document/:id"
+          element={
+            <ProtectedRoute>
+              <DocumentPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
     </>
