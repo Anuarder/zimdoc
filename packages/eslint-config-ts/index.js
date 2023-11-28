d20abfba59e7bfd570c0a025dfc9164e6b46fc73
Anuar Ibrayev <a.ibrayev@chocotravel.com>
2023-11-28 16:17:59 +0600

    chore(eslint-config-ts): setup eslint-config-ts package

---
+++ packages/eslint-config-ts/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: ['plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unsafe-member-access': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/unbound-method': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
